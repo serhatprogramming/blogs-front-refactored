f169936cd5b733a102f99978a15999dab589d111
serhatprogramming <serhatprogramming@gmail.com>
2023-04-19 17:11:25 -0400

    notification reducer added.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+
+import notificationReducer from "./reducers/notificationReducer";
+
+const store = configureStore({
+  reducer: {
+    notifications: notificationReducer,
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
---
+++ src/reducers/notificationReducer.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const notificationSlice = createSlice({
+  name: "notifications",
+  initialState: "",
+  reducers: {
+    addNotification(state, action) {
+      return action.payload;
+    },
+    removeNotification(state, action) {
+      return "";
+    },
+  },
+});
+
+export const { addNotification, removeNotification } =
+  notificationSlice.actions;
+
+export default notificationSlice.reducers;
